893bebf7de28046b20ff36fc9557ab19502d2e3b
M Wildan F <wildanfd11@gmail.com>
2022-08-18 20:37:55 +0700

    Menerapkan readline untuk memasukkan input dari user dan mengeluarkan output

---
+++ nodejs-basic/filesystem/readLine.js
@@ -0,0 +1,27 @@
+const readLine = require("readline");
+
+/** Menggunakan ReadLine dari core module readline */
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Satu pertanyaan
+// rl.question("Masukkan Nama anda : ", (nama) => {
+//   console.log(`Selamat datang kembali, ${nama}`);
+//   rl.close();
+// });
+
+// Tiga pertanyaan
+rl.question("Masukkan nama anda : ", (nama) => {
+  rl.question("Masukkan pesan anda : ", (pesan) => {
+    rl.question("Masukkan email anda : ", (email) => {
+      console.log(
+        `Hai ${nama}, anda mengirimkan pesan : ${pesan}, dengan email : ${email}`
+      );
+      rl.close();
+    });
+  });
+});
+
+module.exports = rl;
