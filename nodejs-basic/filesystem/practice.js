88ed751aaf9ab338e801b6ce496890b04360ab9f
M Wildan F <wildanfd11@gmail.com>
2022-08-19 10:08:40 +0700

    Menambahkan dokumentasi pada bagian readline

--- nodejs-basic/filesystem/practice.js
+++ nodejs-basic/filesystem/practice.js
@@ -2,15 +2,17 @@ const fs = require("fs");
 const readLine = require("readline");
 const path = require("path");
 
+// Membuat interface yaitu input untuk mengambil input dari user, dan output untuk menampilkan input dari user
 const rl = readLine.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const writePath = path.resolve(__dirname, "isiPesan.txt");
+// const writePath = path.resolve(__dirname, "isiPesan.txt"); // format txt
+const anotherPath = path.resolve(__dirname, "pesan.json");
 
 rl.question("Masukkan pesan anda : ", (pesan) => {
   console.log(pesan);
-  fs.writeFileSync(writePath, pesan);
+  fs.writeFileSync(anotherPath, JSON.stringify(pesan)); // JSON.stringify : mengubah string ke dalam bentuk JSON
   rl.close();
 });
