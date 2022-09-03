d5c77e70767f81c91e0cf46e7151de17bc779576
M Wildan F <wildanfd11@gmail.com>
2022-09-03 11:53:16 +0700

    Membuat contact app via command line menggunakan yargs part 1

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,13 +1,36 @@
-// * Mengimport function yang dibutuhkan
-const { buatPertanyaan, simpanContact } = require("./contacts");
+// Import module yargs
+const yargs = require("yargs");
 
-// * Main
-const main = async () => {
-  const nama = await buatPertanyaan("Masukkan nama anda : ");
-  const email = await buatPertanyaan("Masukkan email anda : ");
-  const nomorHP = await buatPertanyaan("Masukkan No HP anda : ");
+// Membuat command
+yargs.command({
+  command: "add",
+  describe: "Menambahkan contact baru",
+  builder: {
+    nama: {
+      describe: "Nama Lengkap", // Keterangan
+      demandOption: true, // Apakah wajib diisi atau tidak
+      type: "string", // Tipe data yang ingin dimasukkan
+    },
+    email: {
+      describe: "Email",
+      demandOption: false,
+      type: "string",
+    },
+    noHP: {
+      describe: "Nomor HP",
+      demandOption: true,
+      type: "string", // karena angka pertama pada no hp adalah '0', maka tidak bisa kita set typenya ke number
+    },
+  },
+  handler(argv) { 
+    // Membuat object untuk menampung data yang diinput lewat command line
+    const contact = {
+      nama: argv.nama,
+      email: argv.email,
+      noHP: argv.noHP
+    };
+    console.log(contact);
+  }
+})
 
-  simpanContact(nama, email, nomorHP);
-};
-
-main();
+yargs.parse();
